d321cb8d4d11ce7b07cf7c772cd6b04e95f161bb
mmartin46 <mmartin46@angelo.edu>
2024-01-06 09:23:52 -0600

    Added music files

--- Game/src/application/Main.java
+++ Game/src/application/Main.java
@@ -8,8 +8,10 @@ import utils.GameConfigurations;
 import utils.Header;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
 import javafx.animation.Timeline;
+import input.InputManager;
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 
@@ -24,6 +26,11 @@ public class Main extends Application {
 	private Header header;
 	private Scene scene;
 	
+	
+	// Check for pause
+	private long lastTimePauseWasPressed;
+	private static final long DELAY_PAUSE_TIME = 800;
+	
 	final int SCREEN_WIDTH = GameConfigurations.SCREEN_WIDTH;
 	final int SCREEN_HEIGHT = GameConfigurations.SCREEN_HEIGHT;
 	
@@ -35,12 +42,13 @@ public class Main extends Application {
 		scene = new Scene(rootPane, SCREEN_WIDTH, SCREEN_HEIGHT);
 		setPixelatedScaling(scene);
 		primaryStage.setScene(scene);
+		initializeInputManager();
+
 		
 		initializeGame(scene);
 		
 		//primaryStage.setResizable(false);
 		primaryStage.show();
-		
 		
 		intitalizeGameLoop();
 	}
@@ -53,13 +61,29 @@ public class Main extends Application {
 		gameLoop.play();
 	}
 	
-	
-	
 	private void initializeGame(Scene scene) {
 		initializeGroups();
 		game = new Game(scene);
 		initializeHeader();
 	}
+	
+	private void initializeInputManager() {
+		InputManager.getSingleton().initialize(scene);
+	}
+	
+	public void checkForKeyPressed() {
+		long currentTime = System.currentTimeMillis();
+		
+		if (InputManager.getSingleton().isKeyPressed(KeyCode.P) &&
+			(currentTime - lastTimePauseWasPressed >= DELAY_PAUSE_TIME)) {
+			
+			lastTimePauseWasPressed = currentTime;
+			
+			boolean currentState = game.getAllowGameRun();
+			game.setAllowGameRun(currentState == true ? false : true);
+		}
+	}
+	
 	
 	private void setPixelatedScaling(Scene scene) {
 		scene.getRoot().setStyle("-fx-image-rendering: optimize-speed; -fx-aliasing: pixelated;");
@@ -82,6 +106,8 @@ public class Main extends Application {
 	
 
 	private void update() {
+		checkForKeyPressed();
+		
 		header.update();
 		game.update();
 		draw();
